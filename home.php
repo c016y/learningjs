aa0b90983ba39e6c7db6a387001135885f85aa4b
Colby Pierce <c016y7@gmail.com>
2019-01-08 21:49:31 -0700

    added content

--- home.php
+++ home.php
@@ -4,6 +4,15 @@
 	<title>Test</title>
 </head>
 <body>
-
+	<div id="title">
+		<p>
+			My name is <h3>Colby Pierce</h3> and I aspire to be a <b>Junior Developer</b> <br>
+			This is my website to show you what I know.
+			<h5><a href="https://github.com/c016y">Heres a link to my GitHub</a></h5>
+		</p>
+	</div>
+	<h3>Javascript</h3>
+	<button onclick="<script src="/website/practice1.js"></script></button>
+	
 </body>
 </html>

9c8a5c196918efa6e46747cdd3f96d614cc35f76
Colby Pierce <c016y7@gmail.com>
2019-01-10 23:03:44 -0700

    more practice, arrays, equality and inequality, scopes, a switch, and a ternary operator

--- scratchsheet.js
+++ scratchsheet.js
@@ -1,5 +1,6 @@
-'use strict';
-// 01-08-2018
+"use strict";
+
+// 01-08-2019
 /* var cash = 10;
 
 var milk = 2.75;
@@ -17,7 +18,7 @@ alert("Your total is" + " " + "$" + products);
  
  alert("Your change is" + " " + "$" + (cash-products)); */
 
-// 01-09-2018
+// 01-09-2019
  /* var a;
  alert(typeof a); //"undefined"
 
@@ -51,12 +52,64 @@ alert("Your total is" + " " + "$" + products);
 	}
 };
 
-//this code does not work for some reason, chrome console says sytax-error on creatEmployeeObject's closing curly
-function createEmployeeObject(firstName, gender, department) {
-	this.firstName = firstName,
-	this.gender = gender,
-	this.department = department,
-	};
+// 01/10/2019
 
-var emp1 = new createEmployeeObject("Katherine", "F", "Accounting");
-*/
+//first array
+
+var arr = [
+	"Hello, World!",
+	99,
+	false
+];
+
+var a = "Hello, World!";
+var b = 3.14159264358;
+
+alert(a.length); //13
+
+//equality / ineqaulity
+var a = "22";
+var b = 22;
+var c = 23;
+
+alert(a == b); //true
+alert(a === b); //false
+alert(a != b); //flase
+alert(a !== b); //true
+
+
+alert(a < c); //true
+
+//scopes
+function foo() {
+	var a = 5;
+
+	if (a >= 5) {
+		let b =4
+
+		while (b < 8) {
+			let c = b * 2;
+			b++;
+
+			alert(a + c);
+		}
+	}
+}
+ foo(); //13 15 17 19
+
+//switch
+var a = 0;
+
+switch (a) {
+	case 1:
+	alert("fail");
+	break;
+	case 2:
+	alert("success");
+	break;
+	default:
+	alert("defaulted");
+}
+//ternary operator
+var b = (a > 1) ? "true1" : "false2";
+ */
